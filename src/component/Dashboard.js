9423b19aeb6e23b7f7d9a20e1bed4ea9991ff15a
liudonghua123 <liudonghua123@gmail.com>
2017-07-12 10:46:27 +0800

    fix small screen dashborad missing nav bar

--- src/component/Dashboard.js
+++ src/component/Dashboard.js
@@ -1,9 +1,19 @@
 import React from 'react';
 import { Card, CardHeader, CardText } from 'material-ui/Card';
+import withWidth from 'material-ui/utils/withWidth';
+import { AppBarMobile } from 'admin-on-rest';
 
-export default () => (
-    <Card style={{ padding: '2em' }}>
-        <CardHeader title="Welcome to the administration" />
-        <CardText>Lorem ipsum sic dolor amet...</CardText>
-    </Card>
-);
+const Dashboard = (props) => {
+    const { width } = props;
+    return (
+        <div>
+            {width === 1 && <AppBarMobile title="众信红包管理平台" />}
+            <Card style={{ padding: '2em' }}>
+                <CardHeader title="Welcome to the administration" />
+                <CardText>Lorem ipsum sic dolor amet...</CardText>
+            </Card>
+        </div>
+    );
+}
+
+export default withWidth()(Dashboard);
